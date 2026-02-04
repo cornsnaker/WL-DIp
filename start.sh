1988b2b0cd385468d42ec3fae3b6d9002dc93f98
cornsnaker <yashwild@rediffmail.com>
2026-02-04 21:26:39 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -2,10 +2,10 @@
 set -e
 
 # Update package list
-sudo apt-get update
+apt-get update
 
 # Install aria2 and qbittorrent-nox
-sudo apt-get install -y aria2 qbittorrent-nox
+apt-get install -y aria2 qbittorrent-nox
 
 # Activate virtual environment and run the app
 source .venv/bin/activate
